1365954c358365042c3ac56322dbd3c2d5ee619a
Shreyash <81287478+shreyashHake@users.noreply.github.com>
2023-04-24 11:45:34 +0530

    Connected frontend to database

--- src/main/java/com/employeeMan/empmanage/auth/AuthenticationController.java
+++ src/main/java/com/employeeMan/empmanage/auth/AuthenticationController.java
@@ -1,15 +1,14 @@
 package com.employeeMan.empmanage.auth;
 
+import com.employeeMan.empmanage.entity.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
+@CrossOrigin(origins = "http://localhost:4200")
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService service;
@@ -25,5 +24,12 @@ public class AuthenticationController {
             @RequestBody AuthenticateRequest request
     ) {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @GetMapping("/get/{email}")
+    public User getUser(
+            @PathVariable String email
+    ) {
+        return service.getUser(email);
     }
 }
